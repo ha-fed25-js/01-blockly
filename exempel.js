b234a96afc4e0e761241a67fe5fdfda718785ca2
David Andersson <lejonmanen@gmail.com>
2025-10-23 10:56:32 +0200

    Uppgift 1.20

--- exempel.js
+++ exempel.js
@@ -4,13 +4,15 @@
 // console.log(13)
 // console.log(14)
 // console.log(15)
+
 /*
 let index = 1;
 while (index <= 100) {
-  console.log(index)
-  index = index + 1;
+console.log(index)
+index = index + 1;
 }
 */
+
 
 /*
 let x, y, z;
@@ -21,12 +23,39 @@ console.log((x + y + z))  // 12
 console.log((x * y * z))  // 60
 */
 
+/*
 var villkor;
 
 
 villkor = true;
 if (villkor == false) {
-  console.log('Villkoret är sant')
+console.log('Villkoret är sant')
 } else {
-  console.log('Villkoret är falskt')
+	console.log('Villkoret är falskt')
+}
+*/
+
+/*
+let x = 3;
+x = x * 5;  // 15
+if (x > 10) {
+console.log('Stort')
+} else {
+	console.log('Litet')
+}
+*/
+
+// Uppgift 1.20
+let timme = 0;
+while (timme < 24) {
+	if (timme == 12) {
+		console.log('Dags för lunch!')
+	} else if (timme >= 9 && timme <= 11) {
+		console.log('Förmiddagslektion')
+	} else if (timme >= 13 && timme <= 15) {
+		console.log('Eftermiddagslektion')
+	} else {
+		console.log(timme)
+	}
+	timme = timme + 1;
 }
