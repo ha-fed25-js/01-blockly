e1012d8eb0b2ca833386d059b6746d4e23c218e7
David Andersson <lejonmanen@gmail.com>
2025-10-23 10:07:11 +0200

    Blockly saves

---
+++ exempel.js
@@ -0,0 +1,32 @@
+// console.log(10)
+// console.log(11)
+// console.log(12)
+// console.log(13)
+// console.log(14)
+// console.log(15)
+/*
+let index = 1;
+while (index <= 100) {
+  console.log(index)
+  index = index + 1;
+}
+*/
+
+/*
+let x, y, z;
+x = 3;
+y = 4;
+z = 5;
+console.log((x + y + z))  // 12
+console.log((x * y * z))  // 60
+*/
+
+var villkor;
+
+
+villkor = true;
+if (villkor == false) {
+  console.log('Villkoret är sant')
+} else {
+  console.log('Villkoret är falskt')
+}
